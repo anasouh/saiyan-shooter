8b22f7451026942b508322b2dfc6787fc5d79d36
Anas Ouhdda <anas.ouhdda.etu@univ-lille.fr>
2024-02-25 20:33:09 +0100

    Add new views and routes to main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
-console.log('main.js chargé !');
+import HomeView from './HomeView.js';
+import Router from './Router.js';
+import View from './View.js';
+
+const canvasList = document.querySelectorAll(
+	'section.viewContainer > div canvas'
+);
+
+function resizeCanvas(canvas) {
+	if (canvas.classList.contains('fullPage')) {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+	}
+}
+
+canvasList.forEach(canvas => resizeCanvas(canvas));
+window.addEventListener('resize', resizeCanvas);
+
+const homeView = new HomeView(document.querySelector('.home'));
+const gameView = new View(document.querySelector('.game'));
+const creditsView = new View(document.querySelector('.credits'));
+
+const routes = [
+	{ path: '/', view: homeView, title: 'Accueil' },
+	{ path: '/game', view: gameView, title: 'Jeu' },
+	{ path: '/credits', view: creditsView, title: 'Crédits' },
+];
+Router.routes = routes;
+
+Router.navigate(window.location.pathname, true);
+window.onpopstate = () => Router.navigate(document.location.pathname, true);
