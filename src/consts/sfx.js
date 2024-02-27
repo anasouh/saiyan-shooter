0aa874166364831beca03dbbcd686fd0f1414a37
Selim Hamza <selim.hamza.etu@univ-lille.fr>
2024-02-27 18:23:05 +0100

    Add SENZU_BEAN sound effect

--- src/consts/sfx.js
+++ src/consts/sfx.js
@@ -5,3 +5,4 @@ export const FALL_TO_GROUND = PATH + 'FX_19.wav';
 export const PUNCH_1 = PATH + 'FX_40.wav';
 export const PUNCH_2 = PATH + 'FX_41.wav';
 export const ENNEMY_DISINTEGRATION = PATH + 'FX_53.wav';
+export const SENZU_BEAN = PATH + 'FX_59.wav';
