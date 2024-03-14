de1f63b11713cbd489a1320aa76a278e63927b71
Anas Ouhdda <anas.ouhdda.etu@univ-lille.fr>
2024-03-14 16:05:32 +0100

    Update webpack configuration

--- server/index.js
+++ server/index.js
@@ -2,14 +2,27 @@ import express from 'express';
 import http from 'http';
 import addWebpackMiddleware from './middlewares/addWebpackMiddleware.js';
 
+const routesPaths = [
+	'/guide',
+	'/game',
+	'/credits',
+];
+
 const app = express();
 const httpServer = http.createServer(app);
 const fileOptions = { root: process.cwd() };
+const port = process.env.PORT || 8000;
 
 addWebpackMiddleware(app);
 
 app.use(express.static('client/public'));
 
-httpServer.listen(process.env.PORT, () => {
-	console.log(`Server running at http://localhost:${process.env.PORT}/`);
+routesPaths.forEach(path => {
+	app.get(path, (req, res) => {
+		res.sendFile('client/public/index.html', fileOptions);
+	});
+});
+
+httpServer.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}/`);
 });
